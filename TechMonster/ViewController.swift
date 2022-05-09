9042f462b605499904d4080801635d5daa14e57f
YamazakiSota <so-chan.y@ezweb.ne.jp>
2022-05-09 21:40:10 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 山崎颯汰 on 2022/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
